e5ae1ce82ec6514e8fc6310fc8a64ad659145a73
FilipCiesielski <filip.carpenter@gmail.com>
2019-09-10 13:38:42 +0200

    0.0 task done

---
+++ oddaj_rzeczy_playground/src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import React,{Component} from 'react';
+import './App.scss';
+
+function App() {
+  return (
+   null
+  );
+}
+
+export default App;
---
+++ oddaj_rzeczy_playground/src/components/Home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import React,{Component} from 'react';
+
+class Home extends Component{
+    render() {
+        return null
+    }
+}
