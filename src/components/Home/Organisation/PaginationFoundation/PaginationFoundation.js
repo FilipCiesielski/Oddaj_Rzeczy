c3b65e100b76f04ab3ce53212d3693ad0287860c
FilipCiesielski <filip.carpenter@gmail.com>
2019-09-16 13:37:06 +0200

    zmiana katalogów

---
+++ src/components/Home/Organisation/PaginationFoundation/PaginationFoundation.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react';
+
+
+
+class PaginationFoundation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foundation: [
+                {
+                    title: '"Dbam o Zdrowie"',
+                    mission: "Pomoc osobom znajdującym się w ciężkiej sytuacji życiowej.",
+                    givenAway: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+                },
+                {
+                    title: '"Dla Dzieci"',
+                    mission: "Pomoc dzieciom z ubogich rodzin.",
+                    givenAway: "ubrania, meble, zabawki"
+                },
+                {
+                    title: '"Bez Domu"',
+                    mission: "Pomoc dla osób nie posiadających miejsca zamieszkania.",
+                    givenAway: "ubrania, jedzenie, ciepłe koce"
+                },
+                {
+                    title: '"Dbam o Zdrowie2"',
+                    mission: "Pomoc osobom znajdującym się w ciężkiej sytuacji życiowej.2",
+                    givenAway: "2ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+                },
+                {
+                    title: '"Dla Dzieci2"',
+                    mission: "Pomoc dzieciom z ubogich rodzin.2",
+                    givenAway: "2ubrania, meble, zabawki"
+                },
+                {
+                    title: '"Bez Domu 2"',
+                    mission: "Pomoc dla osób nie posiadających miejsca zamieszkania.2",
+                    givenAway: "2ubrania, jedzenie, ciepłe koce"
+                },
+                {
+                    title: '"Dbam o Zdrowie3"',
+                    mission: "Pomoc osobom znajdującym się w ciężkiej sytuacji życiowej.3",
+                    givenAway: "3ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+                },
+                {
+                    title: '"Dla Dzieci3"',
+                    mission: "Pomoc dzieciom z ubogich rodzin.3",
+                    givenAway: "3ubrania, meble, zabawki"
+                },
+                {
+                    title: '"Bez Domu 3"',
+                    mission: "Pomoc dla osób nie posiadających miejsca zamieszkania.3",
+                    givenAway: "3ubrania, jedzenie, ciepłe koce"
+                },
+            ],
+            currentPage: 1,
+            foundationPerPage: 3,
+            border1: "borderButton",
+            border2: "",
+            border3: ""
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e, index) {
+        this.setState({currentPage: Number(e.target.id)});
+        console.log(e, index);
+        if (index === 1) {
+            this.setState({border1: "borderButton", border2: "", border3: ""});
+        } else if (index === 2) {
+            this.setState({border1: "", border2: "borderButton", border3: ""});
+        } else if (index === 3) {
+            this.setState({border1: "", border2: "", border3: "borderButton"});
+        }
+    }
+
+    render() {
+        const {foundation, currentPage, foundationPerPage} = this.state;
+        const indexOfLastFoundation = currentPage * foundationPerPage;
+        const indexOfFirstFoundation = indexOfLastFoundation - foundationPerPage;
+        const currentFoundations = foundation.slice(indexOfFirstFoundation, indexOfLastFoundation);
+        const renderFoundation = currentFoundations.map((foundation, index) => {
+            return (
+                <ul key={index} className="foundationInfo">
+                    <li><h3>Fundacja {foundation.title}</h3> <p>Cel i misja: {foundation.mission}</p></li>
+                    <span>{foundation.givenAway}</span>
+                </ul>
+            )
+        });
+        return (
+            <>
+                {renderFoundation}
+                <div className="buttons">
+                    <button id={1} className={this.state.border1} onClick={(e) => this.handleClick(e, 1)}>1</button>
+                    <button id={2} className={this.state.border2} onClick={(e) => this.handleClick(e, 2)}>2</button>
+                    <button id={3} className={this.state.border3} onClick={(e) => this.handleClick(e, 3)}>3</button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default PaginationFoundation;
---
+++ src/components/Home/Organisation/PaginationLocal/PaginationLocal.js
@@ -0,0 +1,59 @@
+import React,{Component} from 'react';
+import "./PaginationLocal.scss"
+
+
+class PaginationLocal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            local: [
+                {title: '"Lorem Ipsum 1"', mission: "Risus in hendrerit gravida rutrum quisque.", givenAway: "Sinto, prendo, questa"},
+                {title: '"Lorem Ipsum 2"', mission: "Ultricies mi eget mauris pharetra et ultrices.", givenAway: "Bento, vintre, sed"},
+                {title: '"Lorem Ipsum 3"', mission: "Sed ullamcorper morbi tincidunt ornare massa eget.", givenAway: "Questas, bonevto, sintas"}
+            ],
+            currentPage: 1,
+            foundationPerPage: 3,
+            border1: "borderButton",
+            border2: "",
+            border3: ""
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e, index) {
+        this.setState({currentPage: Number(e.target.id)});
+        console.log(e, index);
+        if (index === 1) {
+            this.setState({border1: "borderButton", border2: "", border3: ""});
+        } else if (index === 2) {
+            this.setState({border1: "", border2: "borderButton", border3: ""});
+        } else if (index === 3) {
+            this.setState({border1: "", border2: "", border3: "borderButton"});
+        }
+    }
+
+    render() {
+        const {local, currentPage, foundationPerPage} = this.state;
+        const indexOfLastFoundation = currentPage * foundationPerPage;
+        const indexOfFirstFoundation = indexOfLastFoundation - foundationPerPage;
+        const currentFoundations = local.slice(indexOfFirstFoundation, indexOfLastFoundation);
+        const renderLocal = currentFoundations.map((local, index) => {
+            return (
+                <ul key={index} className="foundationInfo">
+                    <li><h3>Zbiórka {local.title}</h3> <p>Cel i misja: {local.mission}</p></li>
+                    <span>{local.givenAway}</span>
+                </ul>
+            )
+        });
+        return (
+            <>
+                {renderLocal}
+                <div className="buttons">
+                    <button id={1} className={this.state.border1} onClick={(e) => this.handleClick(e, 1)}>1</button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default PaginationLocal;
