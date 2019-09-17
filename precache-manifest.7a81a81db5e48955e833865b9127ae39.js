b37c48213b259e5ccc7468265aa87908db9a613b
FilipCiesielski <filip.carpenter@gmail.com>
2019-09-18 00:02:39 +0200

    Updates

---
+++ precache-manifest.7a81a81db5e48955e833865b9127ae39.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2813d3a8ffc9298f6d5966d0dbe5c105",
+    "url": "/Project_Oddaj_Rzeczy_React/index.html"
+  },
+  {
+    "revision": "3a2e57642b01b6aa3199",
+    "url": "/Project_Oddaj_Rzeczy_React/static/css/main.17d7e713.chunk.css"
+  },
+  {
+    "revision": "a607b67c04c9ac60e8d0",
+    "url": "/Project_Oddaj_Rzeczy_React/static/js/2.8d4ef1c2.chunk.js"
+  },
+  {
+    "revision": "3a2e57642b01b6aa3199",
+    "url": "/Project_Oddaj_Rzeczy_React/static/js/main.15fc8298.chunk.js"
+  },
+  {
+    "revision": "0e95d8c8312b9502caa7",
+    "url": "/Project_Oddaj_Rzeczy_React/static/js/runtime~main.bedc3943.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Project_Oddaj_Rzeczy_React/static/media/Signature.e54cfcfc.svg"
+  }
+]);
